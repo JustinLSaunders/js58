052efbca0264b8fda058f35c9db8b12d53a92c23
Justin Saunders <justinleesaunders@gmail.com>
2017-05-18 17:09:54 -0400

    iterative work

---
+++ homework/02/random-address.js
@@ -0,0 +1,8 @@
+addressArray = [
+  states = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"] //includes territories and commonwealths
+  // streets = 
+  ]
+
+address = addressArray[0][Math.floor(Math.random() * (states.length + 1))];
+
+console.log(address);
