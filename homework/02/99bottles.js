a72216392683af47d0ccb522e67616139633da0a
Justin Saunders <jls2245@ais.columbia.edu>
2017-05-18 14:25:24 -0400

    messing around

--- homework/02/99bottles.js
+++ homework/02/99bottles.js
@@ -1,9 +1,10 @@
-for (bottleCount = 99, bottleNext = bottleCount - 1; bottleCount >= 1; bottleCount --, bottleNext --) {
-  if (bottleCount > 1) {
-    var lyrics = (bottleCount + " bottles of beer on the wall, " + bottleCount + " bottles of beer. You take one down, pass it around. " + bottleNext + " bottles of beer on the wall.");
-    console.log(lyrics);
-  } else if (bottleCount = 1) {
+for (bottleCount = 99, nextCount = bottleCount - 1; bottleCount >= 1; bottleCount --, nextCount --) {
+  if (bottleCount > 2) {
+    var lyrics = (bottleCount + " bottles of beer on the wall, " + bottleCount + " bottles of beer. You take one down, pass it around. " + nextCount + " bottles of beer on the wall.");
+  } else if (bottleCount == 2) {
+    var lyrics = (bottleCount + " bottles of beer on the wall, " + bottleCount + " bottles of beer. You take one down, pass it around. " + nextCount + " bottle of beer on the wall.");
+  } else if (bottleCount == 1) {
     var lyrics = (bottleCount + " bottle of beer on the wall, " + bottleCount + " bottle of beer. You take one down, pass it around. No bottles of beer on the wall.");
-    console.log(lyrics);
-  }
+  } 
+  console.log(lyrics)
 }
