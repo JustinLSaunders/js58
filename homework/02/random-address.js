3363aad4e5d3bbd1ad750093c6f0017aa45fb231
Justin Saunders <justinleesaunders@gmail.com>
2017-05-19 16:48:13 -0400

    finished random address

--- homework/02/random-address.js
+++ homework/02/random-address.js
@@ -1,8 +1,22 @@
 addressArray = [
+  streets = ["Broadway", "Main Street", "Fifth Avenue", "Jefferson Avenue", "JS Way"],
+
+  cities = ["New York", "Los Angeles", "Walla Walla", "Kalamazoo", "Albuquerque", "Detroit", "Chicago", "Minneapolis", "Orlando"],
+
   states = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"] //includes territories and commonwealths
-  // streets = 
+
   ]
 
-address = addressArray[0][Math.floor(Math.random() * (states.length + 1))];
+var streetNumber = Math.floor((Math.random() * 9999) + 1);
+var street = addressArray[0][Math.floor(Math.random() * (streets.length))];
+var city = addressArray[1][Math.floor(Math.random() * (cities.length))];
+var state = addressArray[2][Math.floor(Math.random() * (states.length))];
+var zip = Math.floor((Math.random() * 99950) + 10001);
+var apt = Math.floor((Math.random() * 2) + 1);
+var aptNumber = Math.floor((Math.random() * 50) + 1);
+
+if (apt == 2) {var address = streetNumber + " " + street + ", Apt. " + aptNumber + ", " + city + ", " + state + ", " + zip;
+} else if (apt == 1) {var address = streetNumber + " " + street + ", " + city + ", " + state + ", " + zip;}
 
 console.log(address);
+console.log(apt);
