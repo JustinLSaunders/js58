9a2d46fb85e2dfbfbe384ac77397bee10b84135e
Justin Saunders <jls2245@ais.columbia.edu>
2017-05-23 18:32:05 -0400

    name change and some refinements

--- homework/04/functions-homework.js
+++ homework/04/functions-homework.js
@@ -1,18 +1,37 @@
 var numberList = [ 1, 5, 2, 20, 7, 90, 3 ];
-var sortedList = []; //My instincts tell me to make sortedList = numberList, but you'll see below why I did not.
 
-function largestValue() {
-  for(i = 0; i <= numberList.length - 1; i++){
-    sortedList.push(numberList[i]); //If I declared sortedList = numberList above and did not take this step, after I run this function, numberList becomes identical to sortedList, and I want it to remain static in global scope for the following functions.
+function largestValue(array) {
+  var sortedList = []; //My instincts tell me to make sortedList = array, but you'll see below why I did not.
+  for(i = 0; i <= array.length - 1; i++){
+    sortedList.push(array[i]); //If I declared sortedList = array above and did not take this step, after I run this function, numberList becomes identical to sortedList, and I want it to remain static in global scope for the following functions.
   };
-  sortedList.sort((function(a, b){return b-a})); //Without this function, numbers look like strings, and the order is 1, 2, 20, 3, 5, 7, 90. Weird. I have no idea why this works. {return b-a} puts it in descending order.
+  sortedList.sort((function(a, b){return b-a})); //Without this function, numbers look like strings, and the ascending order (using {return a-b}) is 1, 2, 20, 3, 5, 7, 90. Weird. I have no idea why this works. {return b-a} puts it in descending order.
+  // return(sortedList[0]);
   return(sortedList[0]);
 };
 
-largestValue();
-console.log(sortedList[0]);
+console.log(largestValue(numberList));
 console.log(numberList);
-console.log(sortedList);
+
+// function inArray(array, number) {
+//   for (i = 0; i <= numberList.length - 1; i++){
+//     if (numberList[i] == number) {
+//       console.log(number + " is present.");
+//     } else {
+//       console.log(number + " is not present.");
+//     }
+//   }
+// };
+
+// inArray(12);
+
+// function inArray(array, number){
+
+// }
+
+// if(inArray(numberList, 3)){
+//   console.log(number + " is present!");
+// }
 
 // function oddPosition() {
 //   for(var i = 0; i <= (numberList.length - 1); i++) {
