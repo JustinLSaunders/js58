47b1a06a86ed25b17aab73be5e43d2843e3e69cc
Justin Saunders <justinleesaunders@gmail.com>
2017-06-03 22:01:05 -0400

    class 07 homework

---
+++ homework/07/app/app.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+  var form = document.getElementById("fizzbuzz_form");
+  var button = document.getElementById("go-button");
+      
+  button.onclick = function fizzbuzz(){
+
+    event.preventDefault();
+
+    var getOldList = document.querySelector("ol");
+
+    if (getOldList != null) {
+      var oldList = document.getElementById("fizzbuzz_list");
+      form.removeChild(oldList)
+    }
+
+    var userInput = document.getElementById("user-input").value;
+    var userInputParse = Number.parseInt(userInput);
+
+    var fizzbuzzList = document.createElement("ol");
+    form.appendChild(fizzbuzzList);
+    fizzbuzzList.setAttribute("id", "fizzbuzz_list");
+
+    for(var i = 1; i <= userInputParse ; i++){
+      if (i % 3 == 0 && i % 5 == 0) {
+        var fizzbuzzLi = document.createElement("li");
+        var fbText = document.createTextNode("FizzBuzz");
+        fizzbuzzList.appendChild(fizzbuzzLi);
+        fizzbuzzLi.appendChild(fbText);
+      } else if (i % 3 == 0) {
+        var fizzbuzzLi = document.createElement("li");
+        var fText = document.createTextNode("Fizz");
+        fizzbuzzList.appendChild(fizzbuzzLi);
+        fizzbuzzLi.appendChild(fText);
+      } else if (i % 5 == 0) {
+        var fizzbuzzLi = document.createElement("li");
+        var bText = document.createTextNode("Buzz");
+        fizzbuzzList.appendChild(fizzbuzzLi);
+        fizzbuzzLi.appendChild(bText);
+      } else {
+        var fizzbuzzLi = document.createElement("li");
+        var number = document.createTextNode("");
+        fizzbuzzList.appendChild(fizzbuzzLi);
+        fizzbuzzLi.appendChild(number);
+      };
+    }
+  };
+};
+  
