70c91297aa821a8c61a9c7e34f51de9d7d2e1b2b
Justin Saunders <justinleesaunders@gmail.com>
2017-06-03 23:15:59 -0400

    changed ol back to ul

--- homework/07/app/app.js
+++ homework/07/app/app.js
@@ -14,29 +14,33 @@ window.onload = function() {
 
     var userInput = document.getElementById("user-input").value;
     var userInputParse = Number.parseInt(userInput);
-    var fizzbuzzList = document.createElement("ol");
+    var fizzbuzzList = document.createElement("ul");
     form.appendChild(fizzbuzzList);
     fizzbuzzList.setAttribute("id", "fizzbuzz_list");
 
     for(var i = 1; i <= userInputParse ; i++){
       if (i % 3 == 0 && i % 5 == 0) {
         var fizzbuzzLi = document.createElement("li");
-        var fbText = document.createTextNode("FizzBuzz");
+        fizzbuzzLi.setAttribute("class", "fizzbuzz_item");
+        var fbText = document.createTextNode(i + " FizzBuzz");
         fizzbuzzList.appendChild(fizzbuzzLi);
         fizzbuzzLi.appendChild(fbText);
       } else if (i % 3 == 0) {
         var fizzbuzzLi = document.createElement("li");
-        var fText = document.createTextNode("Fizz");
+        fizzbuzzLi.setAttribute("class", "fizz_item");
+        var fText = document.createTextNode(i + " Fizz");
         fizzbuzzList.appendChild(fizzbuzzLi);
         fizzbuzzLi.appendChild(fText);
       } else if (i % 5 == 0) {
         var fizzbuzzLi = document.createElement("li");
-        var bText = document.createTextNode("Buzz");
+        fizzbuzzLi.setAttribute("class", "buzz_item");
+        var bText = document.createTextNode(i + " Buzz");
         fizzbuzzList.appendChild(fizzbuzzLi);
         fizzbuzzLi.appendChild(bText);
       } else {
         var fizzbuzzLi = document.createElement("li");
-        var number = document.createTextNode("");
+        fizzbuzzLi.setAttribute("class", "null_item");
+        var number = document.createTextNode(i);
         fizzbuzzList.appendChild(fizzbuzzLi);
         fizzbuzzLi.appendChild(number);
       };
