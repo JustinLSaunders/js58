545e44f9a3e6ecb5db7c109a672008d3adf67581
Justin Saunders <justinleesaunders@gmail.com>
2017-06-28 17:41:28 -0400

    A few additinal tweaks

--- homework/13/app.js
+++ homework/13/app.js
@@ -4,15 +4,18 @@ var apiKey = "9f672f4d107445ee8db2194825da2f38";
 var nprURL = "https://api.npr.org";
 var nprKey = "7uDMeHXVJeu1IxDHZPQgG5eKCL5CINVXxO5eoJhQ";
 
-$( "#dropdown" ).change(function(){
+$('#dropdown').change(function(){
   $('.article-list').empty();
+  $('#news-source').empty();
+
   var source = $("select#dropdown option:checked").val();
-  // var $this = $(this);
-  // $this.button('loading');
+  var sourceText = $("select#dropdown option:checked").text();
+
+  $('#news-source').text(sourceText);
   $('#loader').toggle();
   $(function(){
     $.get(baseURL + "?source=" + source + "&apiKey=" + apiKey, function(data){
-      console.log(data);
+      // console.log(data);
       // setTimeout(function() {
       //   $this.button('reset');
       // });
@@ -26,13 +29,15 @@ $( "#dropdown" ).change(function(){
         // var newTitle = $('<h3>').text(title);
         // var newDescription = $('<p>').text(description);
         var newRow = $('<div>').attr({'class':'row', 'id':'article-row' + i});
-        var imgContainer = $('<div>').attr({'class':'col-xs-5 col-sm-3', 'id':'img-container' + i});
-        var articleContainer = $('<div>').attr({'class':'col-xs-7 col-sm-9', 'id':'article-container' +i});
+        var imgContainer = $('<div>').attr({'class':'col-xs-9 col-sm-5 col-lg-4', 'id':'img-container' + i});
+        var articleContainer = $('<div>').attr({'class':'col-xs-9 col-sm-7 col-lg-8', 'id':'article-container' +i});
         $('.article-list').append(newRow);
         $('#article-row' + i).append(imgContainer, articleContainer);
         $('#img-container' + i).append('<a href="' + url + '" target="_blank"><img alt="" class="article-img" src="' + img + '"></a>')
         $('#article-container' + i).append('<a href="' + url + '" target="_blank"><h3>' + title + '</h3></a><p>' + description + '</p>');
       }
+      $("#dropdown").val($("#dropdown option:first").val());
     });
   });
 });
+
