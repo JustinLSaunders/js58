d9d3d5badc23811490f90725536b9a010360f3b5
Justin Saunders <justinleesaunders@gmail.com>
2017-06-28 13:16:14 -0400

    little tweaks

--- 13/app.js
+++ 13/app.js
@@ -4,18 +4,18 @@ var apiKey = "9f672f4d107445ee8db2194825da2f38";
 var nprURL = "https://api.npr.org";
 var nprKey = "7uDMeHXVJeu1IxDHZPQgG5eKCL5CINVXxO5eoJhQ";
 
-$('#generate-feedr').on("click", function(){
+$( "#dropdown" ).change(function(){
   $('.article-list').empty();
   var source = $("select#dropdown option:checked").val();
-  var $this = $(this);
-  $this.button('loading');
+  // var $this = $(this);
+  // $this.button('loading');
   $('#loader').toggle();
   $(function(){
     $.get(baseURL + "?source=" + source + "&apiKey=" + apiKey, function(data){
       console.log(data);
-      setTimeout(function() {
-        $this.button('reset');
-      });
+      // setTimeout(function() {
+      //   $this.button('reset');
+      // });
       $('#loader').toggle();
       for(var i = 0; i <= 9; i++){
         var title = data.articles[i].title;
@@ -26,11 +26,11 @@ $('#generate-feedr').on("click", function(){
         // var newTitle = $('<h3>').text(title);
         // var newDescription = $('<p>').text(description);
         var newRow = $('<div>').attr({'class':'row', 'id':'article-row' + i});
-        var imgContainer = $('<div>').attr({'class':'col-xs-3', 'id':'img-container' + i});
-        var articleContainer = $('<div>').attr({'class':'col-xs-9', 'id':'article-container' +i});
+        var imgContainer = $('<div>').attr({'class':'col-xs-5 col-sm-3', 'id':'img-container' + i});
+        var articleContainer = $('<div>').attr({'class':'col-xs-7 col-sm-9', 'id':'article-container' +i});
         $('.article-list').append(newRow);
         $('#article-row' + i).append(imgContainer, articleContainer);
-        $('#img-container' + i).append('<a href="' + url + '" target="_blank"><img class="article-img" src="' + img + '"></a>')
+        $('#img-container' + i).append('<a href="' + url + '" target="_blank"><img alt="" class="article-img" src="' + img + '"></a>')
         $('#article-container' + i).append('<a href="' + url + '" target="_blank"><h3>' + title + '</h3></a><p>' + description + '</p>');
       }
     });
